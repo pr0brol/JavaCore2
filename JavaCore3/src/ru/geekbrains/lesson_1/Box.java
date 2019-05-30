7ea56c8e3f6a911b9cb80b15bb4142b71e014f3f
pr0brol <pr0_brol@mail.ru>
2019-05-30 12:48:22 +0300

    homework2

--- JavaCore3/src/ru/geekbrains/lesson_1/Box.java
+++ JavaCore3/src/ru/geekbrains/lesson_1/Box.java
@@ -1,47 +1,29 @@
 package ru.geekbrains.lesson_1;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Box <T extends Fruit>{
 
-    private T obj;
-    String fruitType;
-    ArrayList inBox = new ArrayList();
-    float weightFruit;
+    private List<T> inBox = new ArrayList<>();
 
-    public Box(Fruit fruit){
-        if(fruit.fruitName.equals("fruit")) {
-            System.out.println("Коробка создана");
-            fruitType = fruit.getFruitName();
-            weightFruit = fruit.getFruitWeight();
-            obj = (T) fruit;
-
-        }
-        else {
-            System.out.println("неизвестный фрукт");
-        }
-    }
 
     public void putFruit(T newFruit, int count){
-        if(newFruit.getFruitName().equals(fruitType)){
-            System.out.printf("В коробку добавлено %s фрукта(ов) %s%n", count, newFruit.getFruitName());
-            while (count > 0){
+
+            while (count > 0) {
                 inBox.add(newFruit);
                 count--;
             }
 
-        }
-        else {
-            System.out.println("Коробка не подходит для данного фрукта");
-        }
     }
 
-    public void getWeight(){
-        System.out.printf("Коробка с %s весит %s%n", this.fruitType, this.inBox.size() * weightFruit);
+    public double getWeight(){
+
+        return this.inBox.size() * this.inBox.get(0).getFruitWeight();
     }
 
-    public boolean compare(Box box){
-        if((this.inBox.size())*weightFruit == (box.inBox.size()*box.weightFruit)){
+    public boolean compare(Box<? extends Fruit> box){
+        if((this.inBox.size())*this.inBox.get(0).getFruitWeight() == (box.inBox.size()*box.inBox.get(0).getFruitWeight())){
             return true;
         }
         else {
@@ -49,13 +31,10 @@ public class Box <T extends Fruit>{
         }
     }
 
-    public void getAll(Box box){
-        if(this.fruitType == box.fruitType){
+    public void getAll(Box<T> box){
 
-            this.putFruit(obj, box.inBox.size());
-            box.inBox.clear();
-        }else {
-            System.out.println("В коробках разное содержимое");
-        }
+        inBox.addAll(box.inBox);
+        box.inBox.clear();
+
     }
 }
